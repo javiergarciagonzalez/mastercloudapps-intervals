f4f973c8db47900af4816d9edcefa92c8380d3e4
Javier García González <javier@garciagon.com>
2020-11-29 14:23:42 +0100

    Move equals and hasCode methods from Min/Max to parent class Limit

--- src/main/java/usantatecla/Limit.java
+++ src/main/java/usantatecla/Limit.java
@@ -2,15 +2,37 @@ package usantatecla;
 
 abstract public class Limit {
 
+    protected double value;
+
+    public Limit(double value) {
+        this.value = value;
+    }
+
     abstract public boolean isWithin(double value);
-    
+
     @Override
     public int hashCode() {
-        return -1;
+        final int prime = 31;
+        int result = 1;
+        long temp;
+        temp = Double.doubleToLongBits(value);
+        result = prime * result + (int)(temp ^ (temp >>> 32));
+        return result;
     }
 
     @Override
     public boolean equals(Object obj) {
-        return false;
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Limit other = (Limit) obj;
+        if (Double.doubleToLongBits(value) != Double.doubleToLongBits(other.value))
+            return false;
+        return true;
     }
+
+    public abstract String toString();
 }
