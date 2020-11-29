d3daab72bd12074f1a0b0e531187e586b619bc0a
Javier García González <javier@garciagon.com>
2020-11-29 19:05:29 +0100

    REFACTOR - Use new limitService class in Max & Min classes

--- src/main/java/usantatecla/Max.java
+++ src/main/java/usantatecla/Max.java
@@ -2,13 +2,16 @@ package usantatecla;
 
 public class Max extends Limit {
 
+    private LimitService limitService;
+
     public Max(double value) {
         super(value);
+        this.limitService = new LimitService();
     }
 
     @Override
     public boolean isWithin(double value) {
-        return this.value > value;
+        return this.limitService.maxIsWithin(this.value, value);
     }
 
     @Override
--- src/main/java/usantatecla/Min.java
+++ src/main/java/usantatecla/Min.java
@@ -2,13 +2,16 @@ package usantatecla;
 
 public class Min extends Limit {
 
+    private LimitService limitService;
+
     public Min(double value) {
         super(value);
+        this.limitService = new LimitService();
     }
 
     @Override
     public boolean isWithin(double value) {
-        return this.value < value;
+        return this.limitService.minIsWithin(this.value, value);
     }
 
     @Override
