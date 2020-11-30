86ed326702ffda643ad107ff63bfcf5170b5fb2a
Javier García González <javier@garciagon.com>
2020-11-30 08:17:24 +0100

    REFACTOR - Add overloaded isWithin methods to limitService to be used by Closed limits

--- src/main/java/usantatecla/LimitService.java
+++ src/main/java/usantatecla/LimitService.java
@@ -9,4 +9,12 @@ public class LimitService {
     public boolean isWithin(Max limitOrigin, Limit newLimit) {
         return limitOrigin.value > newLimit.value;
     }
+    
+    public boolean isWithin(ClosedMin limitOrigin, Limit newLimit) {
+        return limitOrigin.value < newLimit.value || limitOrigin.value == newLimit.value;
+    }
+
+    public boolean isWithin(ClosedMax limitOrigin, Limit newLimit) {
+        return limitOrigin.value > newLimit.value || limitOrigin.value == newLimit.value;
+    }
 }
